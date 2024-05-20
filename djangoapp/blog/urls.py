747024a51c38cd7ebc3b9d8ecba3d165f15fb420
Eduardo Costa <educosta.costa@gmail.com>
2024-05-20 20:44:38 +0100

    codeMirror

--- djangoapp/blog/urls.py
+++ djangoapp/blog/urls.py
@@ -1,5 +1,5 @@
 
-from blog.views import created_by, index, page, post
+from blog.views import category, created_by, index, page, post
 from django.urls import path
 
 app_name = 'blog'
@@ -9,5 +9,6 @@ urlpatterns = [
     path('post/<slug:slug>/', post, name='post'),
     path('page/<slug:slug>/', page, name='page'),
     path('created_by/<int:author_pk>/', created_by, name='created_by'),
+    path('category/<slug:slug>/', category, name='category'),
 ]
 
--- djangoapp/blog/views.py
+++ djangoapp/blog/views.py
@@ -38,6 +38,25 @@ def created_by(request, author_pk):
         }
     )
 
+
+def category(request, slug):
+    posts = (
+        Post.objects.get_published()
+        .filter(category__slug=slug)
+        ) 
+
+    paginator = Paginator(posts, POST_PER_PAGE )
+    page_number = request.GET.get("page")
+    page_obj = paginator.get_page(page_number)
+
+    return render(
+        request,
+        'blog/pages/index.html',
+        {
+            'page_obj' : page_obj,
+        }
+    )
+
 def page(request, slug):
     return render(
         request,
