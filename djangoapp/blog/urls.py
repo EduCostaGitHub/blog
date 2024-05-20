e3abf2c3b0a406822bd74d75b9cdf976de88be12
Eduardo Costa <educosta.costa@gmail.com>
2024-05-20 22:17:53 +0100

    search

--- djangoapp/blog/urls.py
+++ djangoapp/blog/urls.py
@@ -1,5 +1,5 @@
 
-from blog.views import category, created_by, index, page, post
+from blog.views import category, created_by, index, page, post, search, tag
 from django.urls import path
 
 app_name = 'blog'
@@ -10,5 +10,7 @@ urlpatterns = [
     path('page/<slug:slug>/', page, name='page'),
     path('created_by/<int:author_pk>/', created_by, name='created_by'),
     path('category/<slug:slug>/', category, name='category'),
+    path('tag/<slug:slug>/', tag, name='tag'),
+    path('search/', search, name='search'),
 ]
 
